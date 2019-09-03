a97fb6f535b4b5b8254544f124a6ae0d3938cbe9
k22616416 <32244224+k22616416@users.noreply.github.com>
2019-09-03 17:27:44 +0800

    Add files via upload

---
+++ YottaCompilerTool/YottaCompilerTool/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace YottaCompilerTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SerialPortList = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.YottaHexPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.HexPath = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.DriveUpdata = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.cmdOutput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SerialPortList
+            // 
+            this.SerialPortList.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.SerialPortList.FormattingEnabled = true;
+            this.SerialPortList.Location = new System.Drawing.Point(13, 44);
+            this.SerialPortList.Name = "SerialPortList";
+            this.SerialPortList.Size = new System.Drawing.Size(137, 25);
+            this.SerialPortList.TabIndex = 0;
+            this.SerialPortList.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MicroBit磁碟路徑";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(13, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(283, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Yotta microbit-samples路徑";
+            // 
+            // YottaHexPath
+            // 
+            this.YottaHexPath.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.YottaHexPath.Location = new System.Drawing.Point(12, 117);
+            this.YottaHexPath.Name = "YottaHexPath";
+            this.YottaHexPath.Size = new System.Drawing.Size(600, 27);
+            this.YottaHexPath.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(618, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(12, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hex路徑";
+            // 
+            // HexPath
+            // 
+            this.HexPath.Font = new System.Drawing.Font("微軟正黑體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.HexPath.Location = new System.Drawing.Point(13, 193);
+            this.HexPath.Name = "HexPath";
+            this.HexPath.Size = new System.Drawing.Size(600, 27);
+            this.HexPath.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(619, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(654, 152);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 68);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "複製";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // DriveUpdata
+            // 
+            this.DriveUpdata.Location = new System.Drawing.Point(156, 44);
+            this.DriveUpdata.Name = "DriveUpdata";
+            this.DriveUpdata.Size = new System.Drawing.Size(47, 23);
+            this.DriveUpdata.TabIndex = 9;
+            this.DriveUpdata.Text = "更新";
+            this.DriveUpdata.UseVisualStyleBackColor = true;
+            this.DriveUpdata.Click += new System.EventHandler(this.DriveUpdata_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(654, 76);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(134, 68);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "編譯";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // cmdOutput
+            // 
+            this.cmdOutput.AllowDrop = true;
+            this.cmdOutput.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cmdOutput.Location = new System.Drawing.Point(12, 226);
+            this.cmdOutput.MaxLength = 9999999;
+            this.cmdOutput.Multiline = true;
+            this.cmdOutput.Name = "cmdOutput";
+            this.cmdOutput.ReadOnly = true;
+            this.cmdOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.cmdOutput.Size = new System.Drawing.Size(776, 212);
+            this.cmdOutput.TabIndex = 11;
+            this.cmdOutput.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmdOutput);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.DriveUpdata);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.HexPath);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.YottaHexPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SerialPortList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "YottaCompilerTool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox SerialPortList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox YottaHexPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox HexPath;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button DriveUpdata;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox cmdOutput;
+    }
+}
+
---
+++ YottaCompilerTool/YottaCompilerTool/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO.Ports;
+using System.IO;
+using System.Diagnostics;
+namespace YottaCompilerTool
+{
+
+    public partial class Form1 : Form
+    {
+        const string BuildHexPath = @"\build\bbc-microbit-classic-gcc\source\microbit-samples-combined.hex";
+        const string DefultPath = @"C:\Users\user\microbit-samples";
+        const string DefultYottaPath = @"C:\yotta\workspace\Scripts";
+        const string HexFileName = @"microbit-samples-combined.hex";
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach(DriveInfo i in allDrives)
+            {
+                SerialPortList.Items.Add(i.RootDirectory);
+            }
+            SerialPortList.Text = SerialPortList.Items[0].ToString();
+            
+            YottaHexPath.Text = BuildHexPath;
+            HexPath.Text = DefultPath + BuildHexPath;
+            
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            YottaHexPath.Text = path.SelectedPath;
+            HexPath.Text = Path.Combine(path.SelectedPath , BuildHexPath);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            File.Copy(HexPath.Text, Path.Combine(SerialPortList.Text, HexFileName), true);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            HexPath.Text = path.SelectedPath;
+            
+        }
+
+        private void DriveUpdata_Click(object sender, EventArgs e)
+        {
+            SerialPortList.Items.Clear();
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            foreach (DriveInfo i in allDrives)
+            {
+                SerialPortList.Items.Add(i.RootDirectory);
+            }
+            SerialPortList.Text = SerialPortList.Items[0].ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+            Process p = new Process();
+            
+
+            p.StartInfo.FileName = "cmd.exe";
+
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true; //不跳出cmd視窗
+            
+            p.Start();
+            p.StandardInput.AutoFlush = true;
+            p.StandardInput.WriteLine("set PATH=%YOTTA_PATH%;%PATH%");
+            p.StandardInput.WriteLine("cmd /K \"cd % HOMEPATH % & C:\\yotta\\workspace\\Scripts\\activate\"");
+            p.StandardInput.WriteLine("cd " + DefultPath);
+            p.StandardInput.WriteLine("yt build");
+            p.StandardInput.WriteLine("exit");
+            p.StandardInput.WriteLine("exit");
+
+            StreamReader reader = p.StandardOutput;//截取输出流
+            string line = reader.ReadLine();//每次读取一行
+            cmdOutput.AppendText(line + "\n");
+            while (!reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+                cmdOutput.AppendText(line + "\n");
+            }
+            
+            p.WaitForExit();
+            p.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
+
